94460e23c0ae471bf5f476eb7190b00a0052696b
sudarsancse <sudarsansarkarcst@gmail.com>
2025-12-15 06:15:49 +0530

    resume update

--- components/Header.jsx
+++ components/Header.jsx
@@ -62,7 +62,7 @@ function Header() {
           initial={{ y: -30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1.2 }}
-          href="/Sudarsan-Resume.pdf"
+          href="/Sudarsan_sarkar_CV.pdf"
           download
           className=" px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
         >
